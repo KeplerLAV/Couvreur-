5c51f3c37f78dd95e9be8b4f581586f18fe1feab
Daniel-Da-Cunha-Gomes <dacunhagomesdaniel@gmail>
2025-12-03 01:09:47 +0100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
+// import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 // Import Client Components uniquement
@@ -51,7 +51,7 @@ export default function RootLayout({
         {/* <Footer /> */}
 
         {/* Analytics */}
-        <Analytics />
+       
       </body>
     </html>
   )
